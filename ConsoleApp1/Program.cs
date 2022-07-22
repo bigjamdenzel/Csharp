8e975578eb6883485956f6178dcc91e49a4663a7
bigjamdenzel <tiago_p_Correia@sapo.pt>
2022-07-22 14:57:07 +0100

    primeira versao programa

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+public class Exercise1
+{
+    public static void Main()
+    {
+        int i;
+       
+
+        Console.WriteLine("The first 10 natural number are:");
+
+        for (i = 1; i <= 10; i++)
+        {
+            Console.Write("{0} ", i);
+        }
+
+        Thread.Sleep(5000);
+    }
+}
